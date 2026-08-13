49c4938ca55f460d94bb073cb2dbef9627fd10d2
Pixies-sama <omoloyehassan18@gmail.com>
2026-08-13 11:02:05 +0100

    13th august 2026

---
+++ operators/operator2.js
@@ -0,0 +1,17 @@
+// Categories of operators 
+// Arithmethic operator
+
+
+// pre increment 
+// post increment
+// pre decrement
+// post decrement
+
+
+let numb = 2;
+console.log(++numb);
+console.log(++numb + numb++ + --numb);
+console.log(numb--);
+console.log(--numb);
+
+
--- operators/operators.js
+++ operators/operators.js
@@ -1,1 +1,78 @@
+// rithmetic Operator
+// Assignment Operator
+// Comparison Operator
+// Logical Operator
 
+let a = 10;
+let b= 15;
+console.log(a + b);
+console.log(b - a);
+
+let c = 40;
+let d = 4;
+console.log(c / d);
+
+let price = 500;
+let quantity = 6;
+console.log(price * quantity);
+
+let score = 100;
+console.log(score);
+
+score++;
+console.log(score);
+
+let score2= 100;
+score2--;
+console.log(score2 );
+console.log(2**10);
+
+// Assignment operator
+// = += -+ *= /+ %=
+let name = 'Malik'
+console.log(name);
+
+let itemPrice = 400;
+itemPrice += 100;
+console.log(itemPrice);
+
+let item = 5;
+item *= 7;
+console.log(item);
+
+let studentScore = 80;
+studentScore -= 60;
+console.log(studentScore);
+
+let amount = 600;
+amount /= 4;
+console.log(amount);
+
+let number = 100;
+number %= 13;
+console.log(number);
+
+console.log(5 == '5');
+console.log(5 === '5');
+console.log( 10 != 5);
+console.log(10 !== '10');
+
+console.log(40 > 10);
+console.log(40 < 10);
+
+console.log(10>= 10);
+console.log(5 <= 10);
+
+// && and
+// || or 
+// !
+
+let age = 5;
+// both condition must be met
+console.log(age >= 18 && age <= 30);
+
+// at least one condition must be true
+console.log(age >= 18 || age <= 30);
+
+console.log(!true);
+console.log(!false);
